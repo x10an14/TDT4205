00123570b682d88645d085c89fd9f30497b16a3a
x10an14 <x10an14@hotmail.com>
2013-03-13 14:17:39 +0100

    Commented out infinite loop code

--- oving4/src/tree.c
+++ oving4/src/tree.c
@@ -96,13 +96,13 @@ void bind_names ( node_t *root ){
                     for(int i = 0; i < declaration->n_children; i++){
                         /*Now we're iterating over all the variable children of the declaration node in this block*/
                         printf("Declaration children[%d] type: %s\n", i, declaration->children[i]->type.text);
-                        node_t *variableList = declaration->children[i]->children[0];
-                        for(int j = 0; j < variableList->n_children; i++){
-                            symbol_t *value = (symbol_t*) malloc(sizeof(symbol_t));
-                            value->stack_offset = -4*cntr;
-                            symbol_insert((char*) variableList->children[j]->data, value);
-                            cntr++;
-                        }
+                        // node_t *variableList = declaration->children[i]->children[0];
+                        // for(int j = 0; j < variableList->n_children; i++){
+                        //     symbol_t *value = (symbol_t*) malloc(sizeof(symbol_t));
+                        //     value->stack_offset = -4*cntr;
+                        //     symbol_insert((char*) variableList->children[j]->data, value);
+                        //     cntr++;
+                        // }
                     }
                 }
                 bind_names(root->children[1]);
