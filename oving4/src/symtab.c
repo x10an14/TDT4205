07cae70954f5217292db9c579d62d653b214a31b
x10an14 <x10an14@hotmail.com>
2013-03-13 13:50:24 +0100

    Added printf's

--- oving4/src/symtab.c
+++ oving4/src/symtab.c
@@ -84,6 +84,9 @@ void scope_remove (void){
 }
 
 void symbol_insert (char *key, symbol_t *value){
+	printf("\nTrying to insert symbol with following values:\nkey: %s\n", key);
+	printf("values_index: %d\n", values_index);
+	printf("depth: %d\n", scopes_index);
 	values_index++;
 	if(values_index == values_size){
 		values_size *= 2;
@@ -92,6 +95,8 @@ void symbol_insert (char *key, symbol_t *value){
 	//Fix the rest of the symbol_t members:
 	value->depth = scopes_index;
 	value->label = STRDUP(key);
+	printf("label: %s\n", value->label);
+	printf("label size: %d\n", strlen(key));
 	values[values_index] = value;
 	ght_insert(scopes[scopes_index], value, strlen(key), value->label);
 	// Keep this for debugging/testing
@@ -101,15 +106,18 @@ void symbol_insert (char *key, symbol_t *value){
 }
 
 symbol_t *symbol_get (char *key){
+	printf("\nTrying to get symbol with following values: \nkey: %s\n", key);
 	symbol_t* result = NULL;
 	int len = strlen(key);
-	int found = 0;
+	printf("key size: %d\n", len);
 	for (int i = scopes_index; i >= 0; i++){
+		printf("Checking scope #: %d\n", i);
 		result = (symbol_t*) ght_get(scopes[i], len, key);
 		if(result != NULL){
-			found = 1;
+			printf("Found value in this scope!");
 			break;
 		}
+		printf("Nothing found in this scope...\n");
 	}
 	// Keep this for debugging/testing
 	#ifdef DUMP_SYMTAB
