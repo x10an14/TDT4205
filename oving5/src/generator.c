506ae6a8750afe48ae019009c5438e0c1daea7a3
x10an14 <x10an14@hotmail.com>
2013-04-18 00:15:47 +0200

    Adding DECLARATION to uncommented code

--- oving5/src/generator.c
+++ oving5/src/generator.c
@@ -146,12 +146,12 @@ void generate(FILE *stream, node_t *root){
              * Declarations:
              * Add space on local stack
              */
-            // if(root->children[0] != NULL){
-            //     node_t *variableList = root->children[0];
-            //     for(int i = 0; i < variableList->n_children; i++){
-            //         instruction_add(SUB,STRDUP("$4"),esp,0,0);
-            //     }
-            // }
+            if(root->children[0] != NULL){
+                node_t *variableList = root->children[0];
+                for(int i = 0; i < variableList->n_children; i++){
+                    instruction_add(SUB,STRDUP("$4"),esp,0,0);
+                }
+            }
             break;
 
         case PRINT_LIST:
