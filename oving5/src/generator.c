32318bfb1d000db0a0b853dec5cbfbb63f25222f
x10an14 <x10an14@hotmail.com>
2013-04-18 00:14:50 +0200

    Commenting out for finding sgfaults2

--- oving5/src/generator.c
+++ oving5/src/generator.c
@@ -234,11 +234,11 @@ void generate(FILE *stream, node_t *root){
              * - Find the variable's stack offset
              * - If var is not local, unwind the stack to its correct base
              */
-            instruction_add(MOVE,STRDUP("(%ebp)"),eax,0,0);
-            for(int i = 0; i < depth - root->entry->depth; i++){
-                instruction_add(MOVE,STRDUP("(%eax)"),eax,0,0);
-            }
-            instruction_add(PUSH,eax,NULL,root->entry->stack_offset,0);
+            // instruction_add(MOVE,STRDUP("(%ebp)"),eax,0,0);
+            // for(int i = 0; i < depth - root->entry->depth; i++){
+            //     instruction_add(MOVE,STRDUP("(%eax)"),eax,0,0);
+            // }
+            // instruction_add(PUSH,eax,NULL,root->entry->stack_offset,0);
             break;
 
         case INTEGER:
